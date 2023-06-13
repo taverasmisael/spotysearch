12eb89a14820feac7c84f819c6ecfcc96f12f043
Misael Taveras <hello@taverasmisael.com>
2023-06-12 21:16:23 -0400

    assets: add images

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import solidPlugin from 'vite-plugin-solid';
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solidPlugin()],
-  server: {
-    port: 4040,
-  },
-  build: {
-    target: 'esnext',
-  },
-});
+  server: { port: 4040 },
+  build: { target: 'esnext' },
+  publicDir: 'public',
+})
